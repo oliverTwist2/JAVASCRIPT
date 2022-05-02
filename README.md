55545ac702b4583b42699b0cd8e4e07cf7d531d5
Oliver Ikegah <oliverikegah@gmail.com>
2022-05-02 20:17:29 +0100

    My first piece of code

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# JAVASCRIPT
-Learning Javascript
+My first readme
---
+++ index.js
@@ -0,0 +1,35 @@
+//IF STATEMENT
+
+console.log("hello world")
+let number = 6
+if(number >5) {
+console.log("Why naw!!!")
+}
+
+//ELSE_IF STATEMENT
+let direction = "DOWN";
+if(direction == "DOWN") {
+console.log("You are going DOWN");
+} else if(direction == "LEFT") {
+  console.log("You are going LEFT");
+} else if(direction == "RIGHT") {
+console.log("You are going RIGHT");
+} else {
+console.log("You are going up");
+}
+
+
+
+
+//FOR LOOPS
+
+
+let namesOfNoisemakers = ["Oliver", "Kevin", "Divine", "Ruth", "Shedrack"];
+
+for (let i=0; i<5; i++) {
+    console.log("Flog", namesOfNoisemakers[i]);
+}
+
+
+//WHILE LOOPS
+
